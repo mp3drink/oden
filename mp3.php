8923e92aa24b6d8f7ae890adf3acc3e7dc9209aa
mp3drink <52222549+mp3drink@users.noreply.github.com>
2019-06-26 07:29:13 +0700

    Add files via upload

---
+++ mp3.php
@@ -0,0 +1,10 @@
+<?php
+ echo 'Siapa anda?\n';
+ echo 'Anda telah menjalankan script saya\n';
+ echo '--------------------------------------\n';
+ echo '|                                    |\n';
+ echo '|             ANDA SUPLUK            |\n';
+ echo '|                                     \n';
+ echo '--------------------------------------\n';
+ echo '...';
+ ?>
